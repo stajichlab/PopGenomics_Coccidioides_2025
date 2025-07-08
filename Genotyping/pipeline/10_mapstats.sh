37ac80942e46c520013261ef523b88b12effa9e5
Jason Stajich <jasonstajich.phd@gmail.com>
2025-07-08 09:00:58 -0700

    generate summary stats

--- Genotyping/pipeline/10_mapstats.sh
+++ Genotyping/pipeline/10_mapstats.sh
@@ -6,6 +6,7 @@ CPU=2
 if [ $SLURM_CPUS_ON_NODE ]; then
   CPU=$SLURM_CPUS_ON_NODE
 fi
+mkdir -p summary_stats
 pushd aln
 parallel -j $CPU samtools flagstat {} \> {.}.flagstat ::: $(ls *.cram)
-grep mapped *.flagstat | grep -v -P 'mate|primary' | perl -p -e 's/\.flagstat:/\t/; s/\s+\+\s+\d+\s+mapped\s+\((\d+\.\d+)\%\s+.+/\t$1/' | sort -k3,3nr > ../aln.mapped.tsv
+grep mapped *.flagstat | grep -v -P 'mate|primary' | perl -p -e 's/\.flagstat:/\t/; s/\s+\+\s+\d+\s+mapped\s+\((\d+\.\d+)\%\s+.+/\t$1/' | sort -k3,3nr > ../summary_stats/aln.mapped.tsv
