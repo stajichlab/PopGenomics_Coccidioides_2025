54d9e3a3d9334b1b1b6433fcc24ef6d54e77c1ba
Jason Stajich <jasonstajich.phd@gmail.com>
2025-07-10 22:04:39 -0700

    not done but some changes to get snpEff to play with the new bcftools out

--- Genotyping/pipeline/08_snpEff.sh
+++ Genotyping/pipeline/08_snpEff.sh
@@ -43,8 +43,8 @@ mkdir -p $SNPEFFOUT
 ## NOTE YOU WILL NEED TO FIX THIS FOR YOUR CUSTOM GENOME
 if [ ! -e $SNPEFFOUT/$snpEffConfig ]; then
   rsync -a $SNPEFFDIR/snpEff.config $SNPEFFOUT/$snpEffConfig
-  echo "# RMUC_NRRLY2510" >> $SNPEFFOUT/$snpEffConfig
-  echo "$SNPEFFGENOME.genome : Rhodotorula mucilaginosa NRRL Y-2510" >> $SNPEFFOUT/$snpEffConfig
+  echo "# $SNPEFFGENOME" >> $SNPEFFOUT/$snpEffConfig
+  echo "$SNPEFFGENOME.genome " >> $SNPEFFOUT/$snpEffConfig
   chroms=$(grep '##sequence-region' $GFFGENOMEFILE | awk '{print $2}' | perl -p -e 's/\n/, /' | perl -p -e 's/,\s+$/\n/')
   echo -e "\t$SNPEFFGENOME.chromosomes: $chroms" >> $SNPEFFOUT/$snpEffConfig
   #echo -e "\t$SNPEFFGENOME.mito_A_fumigatus_Af293.codonTable : Mold_Mitochondrial" >> $SNPEFFOUT/$snpEffConfig
@@ -63,29 +63,15 @@ makeMatrix() {
   POPNAME=$1
   echo "POPNAME is $POPNAME"
   mkdir -p $POPNAME
-  COMBVCF="$TOPFOLDER/$FINALVCF/$PREFIX.$POPNAME.SNP.combined_selected.vcf.gz $TOPFOLDER/$FINALVCF/$PREFIX.$POPNAME.INDEL.combined_selected.vcf.gz"
-  echo "COMBVCF is '$COMBVCF'"
-  for n in $COMBVCF
-  do
-    echo $n
-    st=$(echo $n | perl -p -e 's/\.gz//')
-    if [ ! -f $n ]; then
-      bgzip $st
-    fi
-    if [ ! -f $n.tbi ]; then
-      tabix $n
-    fi
-  done
+  INVCF=$SCRATCH/$PREFIX.$POPNAME.allvariants.vcf
+  COMBVCF="$TOPFOLDER/$FINALVCF/$PREFIX.$POPNAME.SNP.bcf $TOPFOLDER/$FINALVCF/$PREFIX.$POPNAME.INDEL.bcf"
+  bcftools concat -d both -a -Ov -o $INVCF $COMBVCF 
 
   pushd $POPNAME
-  INVCF=$PREFIX.$POPNAME.allvariants_combined_selected.vcf
   OUTVCF=$PREFIX.$POPNAME.snpEff.vcf
   OUTTAB=$PREFIX.$POPNAME.snpEff.tab
   OUTMATRIX=$PREFIX.$POPNAME.snpEff.matrix.tsv
   DOMAINVAR=$PREFIX.$POPNAME.snpEff.domain_variant.tsv
-  if [[ ! -s $INVCF || $TOPFOLDER/$FINALVCF/$PREFIX.$POPNAME.SNP.combined_selected.vcf.gz -nt $INVCF ]]; then
-    bcftools concat -a -d both -o $INVCF -O v $COMBVCF
-  fi
   echo " java -Xmx$MEM -jar $SNPEFFJAR eff -c $TOPFOLDER/$SNPEFFOUT/$snpEffConfig -dataDir $TOPFOLDER/$SNPEFFOUT/data -v $SNPEFFGENOME $INVCF > $OUTVCF"
   java -Xmx$MEM -jar $SNPEFFJAR eff -c $TOPFOLDER/$SNPEFFOUT/$snpEffConfig -dataDir $TOPFOLDER/$SNPEFFOUT/data -v $SNPEFFGENOME $INVCF > $OUTVCF
 
@@ -93,7 +79,7 @@ makeMatrix() {
 
   # this requires python3 and vcf script
   # this assumes the interpro domains were downloaded from FungiDB and their format - you will need to generalize this
-#  $TOPFOLDER/scripts/map_snpEff2domains.py --vcf $OUTVCF --domains $DOMAINS --output $DOMAINVAR
+  #$TOPFOLDER/scripts/map_snpEff2domains.py --vcf $OUTVCF --domains $DOMAINS --output $DOMAINVAR
 
   # this requires Python and the vcf library to be installed
   $TOPFOLDER/scripts/snpEff_2_tab.py $OUTVCF $REFGENOME > $OUTMATRIX
