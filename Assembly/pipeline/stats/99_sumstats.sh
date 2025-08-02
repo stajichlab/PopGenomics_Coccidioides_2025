3ecfd1c72f986f2f6d89b55a56426d4dcf2e1b26
Jason Stajich <jasonstajich.phd@gmail.com>
2025-08-01 19:07:03 -0700

    sort report by genome asm side

--- Assembly/pipeline/stats/99_sumstats.sh
+++ Assembly/pipeline/stats/99_sumstats.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash -l
 #SBATCH -p short
 
-perl scripts/asm_stats_scaffolds.pl scaffolded_genomes > asm_scaffolded_stats.tsv
-perl scripts/asm_stats.pl genomes > asm_stats.tsv
+perl scripts/asm_stats_scaffolds.pl scaffolded_genomes > asm_scaffolded_stats.$$.tsv
+
+(head -n 1 asm_scaffolded_stats.$$.tsv  && tail -n +2 asm_scaffolded_stats.$$.tsv | sort -k3,3nr) > asm_scaffolded_stats.tsv
+
+perl scripts/asm_stats.pl genomes > asm_stats.$$.tsv
+(head -n 1 asm_stats.$$.tsv  && tail -n +2 asm_stats.$$.tsv | sort -k3,3nr) > asm_stats.tsv
+rm -f asm_stats.$$.tsv asm_scaffolded_stats.$$.tsv
